a40d4466f99cd482daac26625c180a9934f6b5d3
John Vanderyde <jcvandermyde@gmail.com>
2025-11-18 14:25:52 -0600

    added decrimenting stock to fridge

--- server/my-app/src/user-inventory/user-inventory.module.ts
+++ server/my-app/src/user-inventory/user-inventory.module.ts
@@ -5,9 +5,10 @@ import { UserInventoryController } from './user-inventory.controller';
 import { UserInventory } from './entities/user-inventory.entity';
 import { User } from 'src/user/user.entity';
 import { Product } from 'src/product/entities/product.entity';
+import { GroceryList } from 'src/grocery-list/entities/grocery-list.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserInventory, User, Product])],
+  imports: [TypeOrmModule.forFeature([UserInventory, User, Product, GroceryList])],
   controllers: [UserInventoryController],
   providers: [UserInventoryService],
   exports: [UserInventoryService],
